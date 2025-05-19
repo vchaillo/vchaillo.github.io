732ad1e84f32dd53aa31624139c846a69f491d7e
Valentin Chaillou <vchaillo@student.42.fr>
2025-05-19 22:39:28 +0200

    disable resume on launch

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent implements OnInit {
-  resumeIsOpen = true;
+  resumeIsOpen = false;
 
   ngOnInit() {
     document.addEventListener('keydown', this.handleKeydown);
