837236d9783d06e69d355caab66780afbb1ad68f
Valentin Chaillou <vchaillo@student.42.fr>
2025-05-20 16:35:20 +0200

    add animation on name + sections

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ResumeComponent } from '../resume/resume.component';
 
@@ -13,7 +13,31 @@ import { ResumeComponent } from '../resume/resume.component';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('mainTitle') mainTitle!: ElementRef;
+  @ViewChild('headerTitle') headerTitle!: ElementRef;
+
+  ngAfterViewInit() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+
+    const scrollToTopLink = document.querySelector('.scroll-to-top-link');
+
+    if (window.scrollY > 50) {
+      scrollToTopLink?.classList.add('shrink');
+    } else {
+      scrollToTopLink?.classList.remove('shrink');
+    }
+  }
+
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 }
+
+
